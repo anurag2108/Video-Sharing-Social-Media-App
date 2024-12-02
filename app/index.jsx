b9e89025e42d2f954c31f508487864a368dedad6
Anurag Naren Kallakunta <anuragnaren2108@gmail.com>
2024-12-02 18:39:47 -0500

    Initial Commit

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text,View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App(){
+    return (
+        <View className = "flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">Basico!</Text>
+            <Link href="/profile" style={{color:'blue'}}>Go to Profile</Link>
+        </View>
+    )
+}
